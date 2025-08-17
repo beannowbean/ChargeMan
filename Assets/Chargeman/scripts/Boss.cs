50b67eb153d1f68fa28de4e9c677d154cb4e7b0a
Byeong mo Kang <59437430+8LLAT@users.noreply.github.com>
2025-08-17 17:46:52 +0900

    패턴 예고 이팩트 중간저장
    2025.8.17

--- Assets/Chargeman/scripts/Boss.cs
+++ Assets/Chargeman/scripts/Boss.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Boss : MonoBehaviour
 {
@@ -7,16 +9,24 @@ public class Boss : MonoBehaviour
     public Sprite BossDamaged;
     public float damagedTime = 0.2f;
     public Animator animator;
+    private Animator circleWarnAnimator;
 
     private SpriteRenderer spriteRenderer;
     private float damagedTimer = 0f;
     private bool isDamaged = false;
+    private bool isDone = false;
+    private bool isDoing = false;
+    //private bool isTesting = false; //패턴 테스트를 위한 변수
+
+    [SerializeField] private GameObject[] warning;
+    [SerializeField] private GameObject[] pattern;
 
     private void Awake()
     {
-        
+
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        circleWarnAnimator = warning[0].GetComponent<Animator>();
     }
 
     private void Update()
@@ -24,26 +34,42 @@ public class Boss : MonoBehaviour
         // 피격 상태일 때 시간 경과 체크
         if (isDamaged)
         {
-            damagedTimer += Time.deltaTime; 
+            damagedTimer += Time.deltaTime;
 
-            if (damagedTimer >= damagedTime) 
+            if (damagedTimer >= damagedTime)
             {
-                spriteRenderer.sprite = normalSprite; 
-                isDamaged = false; 
+                spriteRenderer.sprite = normalSprite;
+                isDamaged = false;
             }
         }
+        /*
+        if (Input.GetKeyDown(KeyCode.P) && isDoing == false)
+        {
+            Transform tr = warning[0].GetComponent<Transform>();
+            tr.localScale = new Vector3(0f, 0f, 1f);
+            isDoing = true;
+            StartCoroutine(CircleWarning());
+        }
+
+        //if (isDoing) StartCoroutine(CircleWarning());
+        if (isDone)
+        {
+            isDone = false;
+            isDoing = false;
+            StopCoroutine(CircleWarning());
+        }*/
     }
 
     public void TakeDamage(int damage)
     {
-        hp -= damage; 
+        hp -= damage;
         Debug.Log("Boss HP: " + hp);
 
         animator.SetTrigger("isDamaged");
 
         if (hp <= 0)
         {
-            Die(); 
+            Die();
         }
     }
 
@@ -51,5 +77,10 @@ public class Boss : MonoBehaviour
     {
         Debug.Log("Boss defeated!");
         Destroy(gameObject);
+    }
+
+    private IEnumerator CircleWarn()
+    {
+        
     }
 }
