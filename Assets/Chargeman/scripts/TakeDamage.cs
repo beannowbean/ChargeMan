7d2edbf62fa47bc0975e867b2e7d8697c457bc81
Byeong mo Kang <59437430+8LLAT@users.noreply.github.com>
2025-08-18 13:58:48 +0900

    간이 원형 패턴 완성
    2025.8.18
    p를 눌러서 테스트

--- Assets/Chargeman/scripts/TakeDamage.cs
+++ Assets/Chargeman/scripts/TakeDamage.cs
@@ -14,10 +14,14 @@ public class TakeDamage : MonoBehaviour
     //https://howudong.tistory.com/41
     void OnTriggerEnter2D(Collider2D collision)
     {
-        PlayerTeleportMovement player = collision.GetComponent<PlayerTeleportMovement>();
-        if (player is not null && player.CompareTag("Player"))
+        Player player = collision.GetComponent<Player>();
+        if (player is not null && collision.CompareTag("Player"))
         {
             player.TakeDamage(damage); // 플레이어에게 1의 피해를 줌
+
+            Destroy(this.gameObject);
         }
+
+        Destroy(this.gameObject);
     }
 }
