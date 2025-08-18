5a5ddd00e8585522d194d5d261d5eeb2f5db4def
Byeong mo Kang <59437430+8LLAT@users.noreply.github.com>
2025-08-18 15:02:06 +0900

    패턴 안보이는 버그 수정

--- Assets/Chargeman/Scripts/System/TakeDamage.cs
+++ Assets/Chargeman/Scripts/System/TakeDamage.cs
@@ -4,20 +4,15 @@ public class TakeDamage : MonoBehaviour
 {
     [SerializeField] private int damage = 1;
 
-    private Rigidbody2D rb;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
     //https://howudong.tistory.com/41
     void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log(collision.name);
         Player player = collision.GetComponent<Player>();
         if (player is not null && collision.CompareTag("Player"))
         {
             player.TakeDamage(damage); // 플레이어에게 1의 피해를 줌
+            Destroy(this.gameObject);
         }
 
         Destroy(this.gameObject);
