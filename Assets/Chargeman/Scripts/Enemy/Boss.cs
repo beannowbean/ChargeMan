f6edea9b6508bdf3ffffc2396577be9199dd9418
moonsun725 <moonsun725@naver.com>
2025-08-19 18:54:33 +0900

    패턴 크기 및 딜레이 조절 기능 추가
    - 메서드 매개변수 추가
    CircleWarn(float x, float y, float patternDelay),
    LineWarn(float x, float y, float patternDelay)
    - 전역 변수 추가
    // 패턴 시전 시 크기 조절용 변수
    private float xSize = 0;
    private float ySize = 0;

--- Assets/Chargeman/Scripts/Enemy/Boss.cs
+++ Assets/Chargeman/Scripts/Enemy/Boss.cs
@@ -19,6 +19,10 @@ public class Boss : MonoBehaviour
     private int patternNum = 0;
     //private bool isTesting = false; //패턴 테스트를 위한 변수
 
+    // 패턴 시전시 크기 조절용 변수
+    private float xSize = 0;
+    private float ySize = 0;
+
     [SerializeField] private GameObject[] warning;
     [SerializeField] private GameObject[] pattern;
 
@@ -47,15 +51,18 @@ public class Boss : MonoBehaviour
         {
             Debug.Log("버튼 눌림");
             isDoing = true;
-            patternNum = 0;
-            StartCoroutine(CircleWarn());
+            patternNum = 0; // 코루틴 안에서 patternNum 바꾸는 건 어떰
+            xSize = ySize = 10;
+            StartCoroutine(CircleWarn(xSize, ySize,1));
         }
 
         if (Input.GetKeyDown(KeyCode.O) && isDoing == false)
         {
             isDoing = true;
             patternNum = 1;
-            StartCoroutine(LineWarn());
+            xSize = 2;
+            ySize = 1;
+            StartCoroutine(LineWarn(xSize, ySize, 1));
         }
 
         //if (isDoing) StartCoroutine(CircleWarning());
@@ -64,7 +71,8 @@ public class Boss : MonoBehaviour
             isDone = false;
             isDoing = false;
             StopAllCoroutines();
-            Instantiate(pattern[patternNum], this.transform);
+            GameObject Pattern = Instantiate(pattern[patternNum], this.transform);
+            Pattern.transform.localScale = new Vector3(xSize, ySize, 0);
         }
     }
 
@@ -88,13 +96,13 @@ public class Boss : MonoBehaviour
     }
 
     //나중에 코루틴 통합할 수 있으면 할 예정
-    private IEnumerator CircleWarn()
+    private IEnumerator CircleWarn(float x, float y, float patternDelay)
     {
         Transform size = warning[0].GetComponent<Transform>();
         //size.localScale = new Vector3(0f, 0f, 1f);
-        while (size.localScale.x < 10.0f)
+        while (size.localScale.x < x)
         {
-            size.localScale += new Vector3(2f * Time.deltaTime, 2f * Time.deltaTime, 0f);
+            size.localScale += new Vector3(patternDelay * 2f * Time.deltaTime, patternDelay * 2f * Time.deltaTime, 0f);
             yield return null;
             //yield return new WaitForSeconds(0.1f);
         }
@@ -103,13 +111,13 @@ public class Boss : MonoBehaviour
         yield return null;
     }
 
-    private IEnumerator LineWarn()
+    private IEnumerator LineWarn(float x, float y, float patternDelay)
     {
         Transform size = warning[1].GetComponent<Transform>();
         //size.localScale = new Vector3(0f, 1f, 1f);
-        while (size.localScale.x < 1.0f)
+        while (size.localScale.x < x)
         {
-            size.localScale += new Vector3(0.2f * Time.deltaTime, 0f, 0f);
+            size.localScale += new Vector3(patternDelay * 0.2f * Time.deltaTime, 0f, 0f);
             yield return null;
         }
         isDone = true;
