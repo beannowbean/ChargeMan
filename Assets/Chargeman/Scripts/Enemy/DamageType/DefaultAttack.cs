21df7a0862ff01d90887d8ce9b5f5078c18a742e
moonsun725 <moonsun725@naver.com>
2025-08-31 05:01:57 +0900

    Revert "기본형 투사체 프리팹 추가"
    This reverts commit 8e0af76cb5a60945945640041b179567515ab07c.

--- Assets/Chargeman/Scripts/Enemy/DamageType/DefaultAttack.cs
+++ Assets/Chargeman/Scripts/Enemy/DamageType/DefaultAttack.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class DefaultAttack : EnemyAttack // 아 그래 이맛이야
+public class DefaultAttack : TakeDamage // 아 그래 이맛이야
 {
     public override void OnTriggerEnter2D(Collider2D collision)
     {
