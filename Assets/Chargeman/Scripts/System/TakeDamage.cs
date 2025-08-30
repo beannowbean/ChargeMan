8e0af76cb5a60945945640041b179567515ab07c
moonsun725 <moonsun725@naver.com>
2025-08-31 04:00:44 +0900

    기본형 투사체 프리팹 추가
    제곧내

--- Assets/Chargeman/Scripts/System/TakeDamage.cs
+++ Assets/Chargeman/Scripts/System/TakeDamage.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 
-public abstract class TakeDamage : MonoBehaviour // 메서드랑 이름 헷갈릴 것 같은데 바꿔도 괜찮을지
+public abstract class EnemyAttack : MonoBehaviour // 메서드랑 이름 헷갈릴 것 같은데 바꿔도 괜찮을지
 {
-    [SerializeField] private int damage = 1;
-    [SerializeField] private float destroyTime = 1.0f; //아마도 패턴에 따라 달라질 것
+    [SerializeField] public int damage = 1;
+    [SerializeField] public float destroyTime = 1.0f; //아마도 패턴에 따라 달라질 것
 
-    [SerializeField] private float knockbackRate = 0;
-    [SerializeField] Vector2 knockbackDir = Vector2.zero;
-    [SerializeField] int effectId = 0;
-    [SerializeField] float friction = 0;
+    [SerializeField] public float knockbackRate = 0;
+    [SerializeField] public Vector2 knockbackDir = Vector2.zero;
+    [SerializeField] public int effectId = 0;
+    [SerializeField] public float friction = 0; // 
 
     void Awake()
     {
-        Destroy(this.gameObject, destroyTime);    
+        if (destroyTime >= 0) // -1일때로 통일해서 무한지속 하나 만들죠?
+            Destroy(this.gameObject, destroyTime);
     }
 
     //https://howudong.tistory.com/41
@@ -21,7 +22,7 @@ public abstract class TakeDamage : MonoBehaviour // 메서드랑 이름 헷갈�
         Debug.Log(collision.name);
         Player player = collision.GetComponent<Player>();
         
-        if (player is not null && collision.CompareTag("Player"))
+        if (player != null)
         {
             player.OnHit(knockbackRate, knockbackDir, effectId, friction); // 일단 임시로 Player.cs에 OnHit 메서드가 들어있음~~~~~
             player.TakeDamage(damage); // 플레이어에게 1의 피해를 줌
