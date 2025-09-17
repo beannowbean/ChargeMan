673b743f344a1af4da939a6897b31e6577b776f3
Byeong mo Kang <59437430+8LLAT@users.noreply.github.com>
2025-09-17 15:47:18 +0900

    Revert "Revert용"
    This reverts commit 384a4bd2b3edcc405488bc906b143e67df43d27e.

--- Assets/Chargeman/Scripts/Enemy/MaceRobotBoss.cs
+++ Assets/Chargeman/Scripts/Enemy/MaceRobotBoss.cs
@@ -173,8 +173,8 @@ public class MaceRobotBoss : MonoBehaviour, IBoss
         _isDoing = false;
         yield break;
     }
-
-    private IEnumerator Laser(Vector3 startRotation, Vector3 endRotation, float turnOnDelay, float turnOffDelay)
+    
+    private IEnumerator Laser( Vector3 startRotation, Vector3 endRotation, float turnOnDelay, float turnOffDelay)
     {
         float currentTime = 0;
         Transform tr = pattern[3].GetComponent<Transform>();
@@ -200,11 +200,5 @@ public class MaceRobotBoss : MonoBehaviour, IBoss
         StartCoroutine(ArcWarn(new Vector3(10, 10, 1f), 40, 40, 41, 140, 0.6f));
 
         yield break;
-    }
-
-    private IEnumerator MaceCrash()
-    {
-        ScaleUsingPattern(0, new Vector3(0f, 0f, 1f), new Vector3(6f, 6f, 1f), 2);
-        yield break; 
     }
 }
